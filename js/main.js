8aed72cadae35f73a7a7fd59b59a2e012e7130c3
Suresh Paudel <sureshpaudel59@gmail.com>
2025-06-10 19:19:13 +0545

    Updated Pages

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.getElementById("join-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thank you for your interest! We will contact you soon.");
+  this.reset();
+});
