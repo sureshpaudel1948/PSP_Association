d23d6ac9f22303e2c7206dcd24248ee499269117
Suresh Paudel <sureshpaudel59@gmail.com>
2025-09-19 14:21:36 +0545

    Updated

--- js/main.js
+++ js/main.js
@@ -9,20 +9,20 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
-  // Dropdown Toggle for About 
-const aboutDropdownToggle = document.getElementById("about-dropdown-toggle");
-const aboutDropdownMenu = document.getElementById("about-dropdown");
+  // Dropdown Toggle for About
+  const aboutDropdownToggle = document.getElementById("about-dropdown-toggle");
+  const aboutDropdownMenu = document.getElementById("about-dropdown");
 
-if (aboutDropdownToggle && aboutDropdownMenu) {
-  aboutDropdownToggle.addEventListener("click", (e) => {
-    e.preventDefault();
-    aboutDropdownMenu.classList.toggle("hidden");
-    aboutDropdownToggle.setAttribute(
-      "aria-expanded",
-      !aboutDropdownMenu.classList.contains("hidden")
-    );
-  });
-}
+  if (aboutDropdownToggle && aboutDropdownMenu) {
+    aboutDropdownToggle.addEventListener("click", (e) => {
+      e.preventDefault();
+      aboutDropdownMenu.classList.toggle("hidden");
+      aboutDropdownToggle.setAttribute(
+        "aria-expanded",
+        !aboutDropdownMenu.classList.contains("hidden")
+      );
+    });
+  }
 
   // Dropdown Toggle for Resources
   const resourcesDropdownToggle = document.getElementById(
@@ -192,4 +192,5 @@ if (aboutDropdownToggle && aboutDropdownMenu) {
       }
     });
   }
+
 });
